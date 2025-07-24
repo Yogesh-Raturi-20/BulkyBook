c6978c9359af28dbc2bdeb8796124fbd4b2c0191
user <user@DESKTOP-06ADKEA>
2025-07-24 16:28:07 +0530

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBook.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int DisplayOrderById { get; set; }
+    }
+}
